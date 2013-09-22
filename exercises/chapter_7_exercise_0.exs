3be99aa2ade79a6747a00b7d4795ef76a454611b
Antonio Carlos da Graça Mota Durão de Souza <ac.desouza@gmail.com>
2013-09-22 15:00:36 -0300

    List sum without an accumulator.

---
+++ exercises/chapter_7_exercise_0.exs
@@ -0,0 +1,13 @@
+defmodule MyList do
+  def sum(list), do: _sum(list, 0)
+
+  # private methods
+  defp _sum([], total), do: total
+  defp _sum([head | tail], total), do: _sum(tail, head+total)
+end
+
+# Without an accumulator
+defmodule MyListAccumulatorFree do
+  def sum([]), do: 0
+  def sum([head | tail]), do: head + sum(tail)
+end
