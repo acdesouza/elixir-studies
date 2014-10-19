46110ab895051037706ce8ec4505cd2346ab6f7c
Antonio Carlos da Graça Mota Durão de Souza <ac.desouza@gmail.com>
2014-10-19 21:58:28 -0200

    Chapter 2 - Exercise: 1

---
+++ exercises/chapter_2_exercise_1.exs
@@ -0,0 +1,15 @@
+# Exercise: PatternMatching-1
+#
+# Which of the following will match?
+# – a=[1,2,3]
+# – a=4
+# – 4=a
+# – [a,b]=[1,2,3]
+# – a=[[1,2,3]]
+# – [a]=[[1,2,3]]
+# – [[a]]=[[1,2,3]]
+
+a = [1, 2, 3]
+a = 4
+4 = a
+a = [[1,2,3]]
