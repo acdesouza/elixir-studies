ed3a6807910056a9ba47eb86b9cf90d168f64e96
Antonio Carlos da Graça Mota Durão de Souza <ac.desouza@gmail.com>
2014-10-19 22:12:48 -0200

    Chapter 2 - Exercise: 3

---
+++ exercises/chapter_2_exercise_3.exs
@@ -0,0 +1,13 @@
+# Exercise: PatternMatching-3
+#
+# If you assume the variable a initially contains the value 2, which of the following will match?
+# – [a,b,a]=[1,2,3]
+# – [a,b,a]=[1,1,2]
+# – a=1
+# – ^a = 2
+# – ^a = 1
+# – ^a=2-a
+a = 2
+
+a = 1
+^a= 2
